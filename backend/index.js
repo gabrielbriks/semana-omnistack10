a10fe63805daf8bb324e0feb42fd2ba74dd19d6a
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-01-15 14:00:14 -0300

    MongoDB - Instalando Mogoose e configurando link de conxão mogo

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,29 @@
 const express  = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://master:@master@cluster0-itqet.mongodb.net/week10?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+/**
+ *  Apartir desse momento meio que cadastramos o json dentro do express, para entender requisições 
+ * que possuem o corpo no formato JSON 
+ * 
+ *  */
+app.use(express.json());
 /**
  * definindo uma rota, onde o primeiro parametro defini a rota e o segundo parametro recebe uma
  * outra função, que recebera dois parametro req e res.
  * 
- * os principais metodos que iremos utilizar para as rotas são:
+ * os principais metodos HTTP que iremos utilizar para as rotas são:
  *  get(usado para listar, buscar por exemplo usuarios), 
  *  post(usado quando queremos criar alguma info, Ex: salvar),
  *  put(usado quando realizamos o update/edição de uma info),
  *  delete(usado para deletar alguma informação)
  *  */ 
-app.get('/',(request, response) => {
+app.post('/users',(request, response) => {
+  console.log(request.body);
   return response.json({ mensage: 'Hello Oministack' });
 });
 

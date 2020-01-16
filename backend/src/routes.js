a8af7ce9f799502279a27d034065df3f2da570dc
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-01-16 17:57:08 -0300

    SearchController - Adicionando novo controller buscar Dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
@@ -8,5 +9,7 @@ const routes = Router();
 routes.get('/devs', DevController.index);
 //rota cadastrar new dev
 routes.post('/devs', DevController.store);
+//rota de busca
+routes.get('/search', SearchController.index);
 
 module.exports = routes;

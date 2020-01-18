ff9b337fd8461556b2876167c3d7cb0d730df583
gabrielbriks <greis9010@gmail.com>
2020-01-18 20:38:35 -0300

    Acesso a API - Instalando cors para para liberação de acesso para o nosso front

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -13,6 +14,12 @@ mongoose.connect('mongodb+srv://master:@master@cluster0-itqet.mongodb.net/week10
  * que possuem o corpo no formato JSON 
  * 
  *  */
+
+ /* o cors pode receber parametro e um deles e o origin, onde podemos definir um endereçõ de acesso 
+  especifico..
+  em nosso caso deixaremos vazio para que esse acesso seja livre 
+ */
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 

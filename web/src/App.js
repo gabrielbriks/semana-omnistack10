113f5804d9cbd2973e0aae90b6dd2d3224594915
gabrielbriks <greis9010@gmail.com>
2020-01-18 20:18:27 -0300

    Interface Web - Concluindo implementação da busca da geolocalização do usuario pelo navegador

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css'
+
+/* Anotações Gerais
+  # UseEfect()
+    Ele serve para dispararmos uma função toda vez que uma informação for alterada,
+    ou também apenas uma unica vez. Ele recebe 2 parametros, o primeiro falamos para
+    ele qual vai ser a função que ele irá executar, e o segundo dizemos QUANDO ele ira
+    executar.
+
+    No segundo parametro dexaremos um vetor vazio, pois se ele estiver vazio ela irá executar
+    apena uma unica vez.
+  #
 
 
+
+*/
 
 /* Os tres conceitos do React
   1#Componente
@@ -40,11 +54,48 @@ import './Sidebar.css';
       São informações mantidas pelo componente (Lembrar: imutabilidade)
 */
 
-/*definicao de App
-  é uma função que retorna um conteudo html;
-  o conteudo html e o que chamamos de JSX
-*/
+  /*definicao de App
+    é uma função que retorna um conteudo html;
+    o conteudo html e o que chamamos de JSX
+  */
 function App() {//Componente PAI
+  
+  //criandao estado para ser setado os valores da geoLocation em nosso inputs de Localização
+  const [ github_username, setGitHubUsername ] = useState('');
+  const [ techs, setTechs ] = useState('');
+  
+  const [ latitude, setLatitude ] = useState('');
+  const [ longitude, setLongitude ] = useState('');
+  
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //funcao de sucesso, retorna a position  do user
+         console.log(position.coords);
+        const { latitude, longitude} = position.coords;
+        
+        console.log(latitude);
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout:30000,
+      }
+    )
+  }, [latitude, longitude, setLatitude, setLongitude]);
+    
+  //Funcao que ira disparar ao clicar no submit
+  async function handleAddDev(e){
+    //Como ele possui um comportamento padrao de levar para outra tela utilizamos o preventDefault
+    e.preventDefault();
+    
+
+  }
 
   return (
    <div id="app">
@@ -53,30 +104,107 @@ function App() {//Componente PAI
       <form>
         <div className="input-block">
           <label htmlFor="github_username">Usuário do GirHub</label>
-          <input name="github_username" id="github_username" required />
+          <input 
+          name="github_username" 
+          id="github_username" 
+          required 
+          value = {github_username}
+          onChange = { e => setGitHubUsername(e.target.value)}
+          />
         </div>
 
         <div className="input-block">
           <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required />
+          <input
+            name="techs" 
+            id="techs" 
+            required 
+            value = {techs}
+            onChange={e => setTechs(e.target.value)}
+          />
         </div>
 
         <div className="input-group">
           <div className="input-block">
             <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude" required />
+            <input 
+              type="number"
+              name="latitude" 
+              id="latitude" 
+              required 
+              value={latitude}
+              onChange={e => setLatitude(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" id="longitude" required />
+            <input 
+              type="number"
+              name="longitude"
+              id="longitude"
+              required 
+              value={longitude}
+              onChange={e => setLongitude(e.target.value)}
+            />
           </div>
         </div>
 
         <button type="submit">Salvar</button>
       </form>
      </aside>
+
      <main>
+      
+      <ul>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/37519878?s=400&u=92a1bf6f06e6729873b86fd21af8c6c29f71c979&v=4" alt="Avatar"/>
+            <div className="user-info">
+              <strong>Gabriel Briks</strong>
+              <span>JavaScript, C#, ReactJS</span>
+            </div>
+          </header>
+          <p>Desenvolvedor C# e Javascript em grande construção...</p>
+          <a href="http://github.com/gabrielbriks">Acessar perfil GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/37519878?s=400&u=92a1bf6f06e6729873b86fd21af8c6c29f71c979&v=4" alt="Avatar"/>
+            <div className="user-info">
+              <strong>Gabriel Briks</strong>
+              <span>JavaScript, C#, ReactJS</span>
+            </div>
+          </header>
+          <p>Desenvolvedor C# e Javascript em grande construção...</p>
+          <a href="http://github.com/gabrielbriks">Acessar perfil GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/37519878?s=400&u=92a1bf6f06e6729873b86fd21af8c6c29f71c979&v=4" alt="Avatar"/>
+            <div className="user-info">
+              <strong>Gabriel Briks</strong>
+              <span>JavaScript, C#, ReactJS</span>
+            </div>
+          </header>
+          <p>Desenvolvedor C# e Javascript em grande construção...</p>
+          <a href="http://github.com/gabrielbriks">Acessar perfil GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/37519878?s=400&u=92a1bf6f06e6729873b86fd21af8c6c29f71c979&v=4" alt="Avatar"/>
+            <div className="user-info">
+              <strong>Gabriel Briks</strong>
+              <span>JavaScript, C#, ReactJS</span>
+            </div>
+          </header>
+          <p>Desenvolvedor C# e Javascript em grande construção...</p>
+          <a href="http://github.com/gabrielbriks">Acessar perfil GitHub</a>
+        </li>
+      </ul>
 
      </main>
    </div>

22f5240be5089045396749efadd6debb928cc82c
gabrielbriks <greis9010@gmail.com>
2020-01-19 19:58:48 -0300

    Corrigindo erro durante response de cadastro de Dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -26,37 +26,36 @@ module.exports = {
         //se caso existir o Dev que esta sendo cadastrado ja se encontra no banco 
         let dev = await Dev.findOne({ github_username });
 
-        if(!dev){
+        if(!dev)
+        {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
-        const { name = login, avatar_url, bio } = apiResponse.data;
-    
-        // console.log(name, avatar_url, bio);
-    
-        const techsArray = parseStringAsArray(techs);
-        
-        // criando esse location para armazenar a latitude e a longitude 
-        const location = {
-          type: 'Point',
-          coordinates: [longitude, latitude]
+          const { name = login, avatar_url, bio } = apiResponse.data;
+      
+          //  console.log(name, avatar_url, bio);
+      
+          const techsArray = parseStringAsArray(techs);
+          
+          // criando esse location para armazenar a latitude e a longitude 
+          const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          }
+      
+          const dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location,
+          });
+          
+          return response.json(dev);
         }
-    
-        const dev = await Dev.create({
-          github_username,
-          name,
-          avatar_url,
-          bio,
-          techs: techsArray,
-          location,
-        });
-    
-        // console.log(techsArray);
-    
-    }
-        return response.json(dev);
       }
-
+        
+        
     //Criar >> async Update;
-
     //Criar >> async Destroy
 };

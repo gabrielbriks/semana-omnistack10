fe065cdde09b9cb505d5c6f1610897bd9a76b7f4
gabrielbriks <greis9010@gmail.com>
2020-01-21 22:07:50 -0300

    Mobile Maps - Adicionando o mapa - Init

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-/* Como nao podemos utilizar a div, utilizamos 
-    essa importação que equivale a div
-*/
-import { View } from 'react-native';
+//para utilizarmos ou melhor dizendo construirmos css para os componets utilizamos essa importação
+import { StyleSheet } from 'react-native';
+
+//importando lib Mapas
+import MapView from 'react-native-maps';
 
 //Criando componente
 function Main(){
-    return <View />
+                        {/* Pq duas chaves? Pq o style ja necessita ter chave, e para declarar o
+                            proprio css que é, e deve ser um objeto, tambem precisa de chaves,
+                        por essa razao temos duas chaves  */}
+    return <MapView  style={{flex:1}}/>
 }
 
+//utilizando a lib importada, para novo style.
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+    },
+});
 
 export default Main;

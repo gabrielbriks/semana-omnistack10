17f50832af45cd8868d21f8942973a1f0a55aed1
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-01-27 13:16:06 -0300

    Mobile - Ajustando link para API hospedada no Heroku

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://192.168.1.4:3333/',
+    baseURL:'https://radar-devs.herokuapp.com',
     
-    //exp://10.101.0.189:19000
 });
 
 export default api;

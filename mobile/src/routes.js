ba100c7638f4ef0a24abd314fc00d5f6f286fc04
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-01-28 13:10:28 -0300

    Ajustando master

---
+++ mobile/src/routes.js
@@ -0,0 +1,47 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+
+//importando as nossas paginas
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+
+/*Mesma abordagem que possuí no docs expo Guide de 'Hello Navigation' 
+Link: https://reactnavigation.org/docs/en/hello-react-navigation.html
+*/
+const Routes = createAppContainer(
+    createStackNavigator({
+        // Objetos com as rotas da nossa aplicação;
+        Main:{
+            //qual componente que sera renderizado
+            screen: Main,
+            navigationOptions:{
+                title: 'Dev Radar',
+            }
+        },
+        Profile:{
+            screen: Profile,
+            navigationOptions:{
+                title: 'Perfil no GitHub'
+            }
+        },
+    },{
+        defaultNavigationOptions:{
+            //Aqui ele aceita todos tipos de styles
+            headerTintColor:'#FFF',            
+            headerTitleAlign:'center',
+            headerBackTitleVisible: false,//Propiedade para retirar o titulo do btn voltar do header, não aplicavel para o android(eu acho); 
+            headerTitleStyle:{
+                fontSize:25,
+                fontWeight: 'bold'
+            },
+            headerStyle:{
+                backgroundColor: '#7b40e7',
+                
+            }
+        }
+    })
+);
+
+
+
+export default Routes;
